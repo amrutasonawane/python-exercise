ea366598840710f938d7ee39948934c860220e6a
amgarud <amgarud@redhat.com>
2026-01-04 15:08:49 +0530

    Adding latest assignment

---
+++ Assignment_4/Question_5.py
@@ -0,0 +1,8 @@
+# Predict the output:
+s = "Python" 
+print (id(s)) 
+s=s+"3" 
+print(id(s))
+
+d = {1: "One", 1: "ONE", 2: "Two"} 
+print(d)
---
+++ Assignment_4/Question_9.py
@@ -0,0 +1,6 @@
+#Predict the output:
+r = range (5)
+print(r)
+print(list(r))
+
+# Output is- range(0, 5) [0, 1, 2, 3, 4]
