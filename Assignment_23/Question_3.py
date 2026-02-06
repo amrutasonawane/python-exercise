e1592cddab9dc40a33bb7dce7a2a07b030225e8f
amgarud <amgarud@redhat.com>
2026-02-06 10:17:55 +0530

    Adding assignment_22 and 23

--- Assignment_23/Question_3.py
+++ Assignment_23/Question_3.py
@@ -11,16 +11,29 @@ class Numbers :
 
 	def checkPerfect(self):
 		number = 0
-		for i in range (1,self.Value // 2):
+		for i in range (1,(self.Value // 2) + 1):
 			if(self.Value % i ) == 0:
 				number = number + i
-				print(number)
 		if(number == self.Value):
 			return True
 		else:
 			return False
 
-obj1 = Numbers(28)
+	def getFactors(self):
+		allFactors = []
+		for i in range (1,(self.Value // 2) + 1):
+			if(self.Value % i) == 0 :
+				allFactors.append(i)
+		return allFactors
+
+	def sumFactors(self):
+		sum = 0
+		allFactors = self.getFactors()
+		for i in range(len(allFactors)):
+			sum = sum + allFactors[i]
+		return sum
+
+obj1 = Numbers(15)
 if(obj1.checkPrime()):
 	print("Given number is prime")
 else:
@@ -30,4 +43,9 @@ if(obj1.checkPerfect()):
 	print("Given number is perfect number")
 else:
 	print("Given number is not perfect number")
-		
+
+Result = obj1.getFactors()
+print("Factors of given number is : ",Result)
+
+sumOfFactors = obj1.sumFactors()
+print("Sum of all factors of given number is : ",sumOfFactors)	
